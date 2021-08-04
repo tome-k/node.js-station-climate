5288cf7bdd407e44ae15806490ddb206b42f7d7b
nicholas-bd <nicholas.bond.d@gmail.com>
2021-08-04 17:10:40 +0200

    refactored code ,using es6 destructure syntax

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=289ddf43e348b124b6778985d3f2aeb1&query=${latitude},${longitude}`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
       callback(
         undefined,
-        `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees It's feels like ${response.body.current.feelslike} degrees out`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees It's feels like ${body.current.feelslike} degrees out`
       );
     }
   });
