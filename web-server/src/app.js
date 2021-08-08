e32e0a407bc63c7695da5a0f40da2e82439660cf
nicholas-bd <nicholas.bond.d@gmail.com>
2021-08-08 20:17:11 +0200

    added query string functionality

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,9 +40,27 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
   res.send({
     forecast: "Sunny and funny",
     location: "Cracow",
+    address: req.query.address,
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  res.send({
+    products: [],
   });
 });
 
