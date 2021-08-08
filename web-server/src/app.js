2056f99031c10227f0a4fb80199cd4baa6f71051
nicholas-bd <nicholas.bond.d@gmail.com>
2021-08-08 19:51:51 +0200

    added new files front-end

---
+++ web-server/src/app.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Mario Golik",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Mario Golik",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Mario Golik",
+    message: "This is message from help page!",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "Sunny and funny",
+    location: "Cracow",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Mario Golik",
+    errorMessage: "Help article Not Found!",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Mario Golik",
+    errorMessage: "Page Not Found!",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000...");
+});
