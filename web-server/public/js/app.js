96f9e9cb6a9f35a4edcf66f9216bd38d37f71405
nicholas-bd <nicholas.bond.d@gmail.com>
2021-08-10 12:42:50 +0200

    removed unusless files, ready to deploy

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,24 @@ console.log("Client side Js file is loaded");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const msgOne = document.querySelector("#msg-1");
+const msgTwo = document.querySelector("#msg-2");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
   const url = `http://localhost:3000/weather?address=${location}`;
 
+  msgOne.textContent = "Loading...";
+  msgTwo.textContent = "";
+
   fetch(url).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error);
+        msgOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
       }
     });
   });
