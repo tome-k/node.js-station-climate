bfc5961865c16e37b72e34e21a58eb9a6b5e1bed
nicholas-bd <nicholas.bond.d@gmail.com>
2021-08-10 10:42:18 +0200

    added fetching data functionality

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log("Client side Js file is loaded");
+
+const url = "http://localhost:3000/weather?address=boston";
+fetch(url).then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
