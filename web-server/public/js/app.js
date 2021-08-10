005389a99ad44009aeb5d30c5aebc1fc03d8d9f6
nicholas-bd <nicholas.bond.d@gmail.com>
2021-08-10 11:59:07 +0200

    added form submit and wired up to UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,21 @@
 console.log("Client side Js file is loaded");
 
-const url = "http://localhost:3000/weather?address=boston";
-fetch(url).then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  const url = `http://localhost:3000/weather?address=${location}`;
+
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
   });
 });
